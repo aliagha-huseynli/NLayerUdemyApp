ce3833800fef4c1339eca7806fc558981f428cd5
Aliagha Huseynli <aliagha.huseynli@gmail.com>
2022-12-07 15:38:12 +0400

    Add project files.

---
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Models;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category { Id = 1, Name = "Pen" },
+                new Category { Id = 2, Name = "Book" },
+                new Category { Id = 3, Name = "Ruler" });
+        }
+    }
+}
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Models;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(new Product
+            {
+                Id = 1,
+                CategoryId = 1,
+                Name = "Pen 1",
+                Price = 200,
+                Stock = 30,
+                CreatedDate = DateTime.Now
+            },
+            new Product
+            {
+                Id = 2,
+                CategoryId = 1,
+                Name = "Pen 2",
+                Price = 300,
+                Stock = 15,
+                CreatedDate = DateTime.Now
+            },
+            new Product
+            {
+                Id = 3,
+                CategoryId = 2,
+                Name = "Pen 3",
+                Price = 500,
+                Stock = 5,
+                CreatedDate = DateTime.Now
+            });
+        }
+    }
+}
